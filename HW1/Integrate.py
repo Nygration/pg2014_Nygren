4a0c69be21031f345a91f8127933e74780802e54
Nygration <christian.j.nygren@gmail.com>
2014-09-23 13:32:53 -0500

    Create Integrate.py

---
+++ HW1/Integrate.py
@@ -0,0 +1,9 @@
+"""
+Created on Fri Sep 19 13:37:12 2014
+@author: Nygren
+part 2 write a function that computes the integral of a list of numers by the trapazoidal rule with a default value of dx=1.0 
+"""
+def integrate(f,dx=1.0):
+    """This function will take a list of numbers as an argument f, and integrate using the trapezoidal rule """
+    print (dx*sum(f)-((f[0]+f[-1])/2)) # trapazoidal just adds a half the sum of two numbers, and runs through so it adds two halves of each number except the fist and last. 
+        
